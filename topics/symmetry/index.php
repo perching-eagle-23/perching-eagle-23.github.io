89c0958e1dc1f21a0912cba21d852c0b3d4b01a5
perching-eagle-23 <whom42@gmail.com>
2021-03-30 18:47:41 -0700

    Rotations working

--- topics/symmetry/index.php
+++ topics/symmetry/index.php
@@ -1,5 +1,5 @@
  <?php 
-      $pagetitle = 'Topics / Triangles'; 
+      $pagetitle = 'Topics / Symmetry'; 
       $pagescripts = "<!-- MathJax --> 
          <script async src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
          <script defer src='symmetry.js' type='text/javascript'></script>"; 
@@ -18,13 +18,66 @@
       <div id="sub1content">
          <p>How are snowflakes so diverse, and yet so many have six sides?  Why are table salt crystals, and many other mineral crystals, cubic?  This question led Johannes Kepler to the first really solid (ha!) argument that matter is atomic; that it is built of building blocks which are, except in extreme physical circumstances, indivisible. {build : page from Strena seu de Nive Sexangula} By constructing large solids out of regular building blocks that lock together at regular angles, we can put the blocks together in any of quadrillions of ways, and still arrive at large shapes which are similar.  Actually, this is what "crystal" means - a solid in which the constituent atoms are arranged periodically, ie in a repeating pattern.  (Periodicity, which we study with the unit circle and trigonometric functions, is an instance of translational symmetry). {build : d3 circle packing with buttons for the different symmetry operations corresponding to animated transitions}</p>
          
+         <!--
          <div id="circles" style="border:3px solid black; width:400px; height:400px;">
             <svg id="circleholder"></svg>
          </div>
-         
          <button id="squareRef1">Reflect vertical</button>
          <button id="squareRot1">Rotate counterclockwise</button>
-
+         -->
+         
+         <div class="figure">
+            <h3 class="figure__title">Symmetry transforms</h3>
+            <div class="figure__image figure__image2">
+               <div>
+                  <div style="text-align: center;">
+                     <svg id="squareGroup" width="75%" height="75%" viewBox="0 0 100 100">
+                     <!--
+                        <rect height="100%" width="100%"></rect>
+                        <circle cx="50" cy="50" r="20" fill="blue"></circle>
+                        -->
+                     </svg>
+                     <div class="figure__label button_label8">
+                        <button>r 90&deg</button>
+                        <button>r 180&deg</button>
+                        <button>r 270&deg</button>
+                        <button>r 360&deg</button>
+                  
+                        <button>m 90&deg</button>
+                        <button>m 180&deg</button>
+                        <button>m 270&deg</button>
+                        <button>m 360&deg</button>
+                     </div>
+                  </div>
+               </div>
+               <div>
+                  <div style="text-align: center;">
+                     <svg id="hexagonGroup" width="75%" height="75%" viewBox="0 0 100 100">
+                     <!--
+                        <rect height="100%" width="100%"></rect>
+                        <circle cx="50" cy="50" r="20" fill="blue"></circle>
+                        -->
+                     </svg>
+                     <div class="figure__label button_label12">
+                        <button id="tom">r 60&deg</button>
+                        <button>r 120&deg</button>
+                        <button>r 180&deg</button>
+                        <button>r 240&deg</button>
+                        <button>r 300&deg</button>
+                        <button>r 360&deg</button>
+                        <button>m 60&deg</button>
+                        <button>m 120&deg</button>
+                        <button>m 180&deg</button>
+                        <button>m 240&deg</button>
+                        <button>m 300&deg</button>
+                        <button>m 360&deg</button>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <p class="figure__label">The fundamental transformations of squares and hexagons that leave them unchanged.  These collections of operations are called <a href="https://en.wikipedia.org/wiki/Dihedral_group" target="_blank">dihedral groups</a>. "r 60&deg" stands for rotate, "m 60&deg" for mirror/reflect.</p>
+         </div>
+         
          <p>To get direct evidence of this, we shot X-rays through cyrstals, and got these patterns out the other side.  {build : simple cubic and hcp diffraction patterns} These images are in some sense the geometric "inverse" of the actual arrangment of the atoms; still, the symmetry survives this transformation.  (This is an example of an extremely powerful area of study called the inverse scattering problem, which describes optics, collisions, particle interactions, and much more).  For the first time, atomicity was no longer a philosophical quandry: symmetry in X-ray projections closed the case.</p>
          
       </div>
--- topics/symmetry/symmetry.js
+++ topics/symmetry/symmetry.js
@@ -1,11 +1,23 @@
 // Create data: coordinates of start and end
-var legLength = 10; 
-var origin = [75,75]; 
+
+var origin = [50,50];
+var circleRadius =  10; 
 
 window.onload = function(){
    drawCircles();
    attachNavHandlers();
-   console.log('what');
+    
+   d3.select("#tom").on("click", function() {
+      var operations = this.innerText; 
+      rotateGroup(this.innerText, ".hexagonGroupToMove"); 
+   });
+   
+   d3.select(".button_label8").selectAll("button").on("click", function() {
+      var operations = this.innerText; 
+      rotateGroup(this.innerText, ".squareGroupToMove");
+      rotateGroup(this.innerText, ".squareGroupToMove");
+   }); 
+   
    d3.select("#squareRef1").on("click", reflect); 
    d3.select("#squareRot1").on("click", function() {
       d3.selectAll("g")
@@ -20,37 +32,103 @@ window.onload = function(){
             .attr("transform-origin", "75 75")
             .attr("transform", "scale(-1,1)"); 
    }); 
-   console.log(changeOrigin([75,75])); 
-   console.log(changeOrigin([0,0], false));
-   console.log(changeOrigin([75,75], false)); 
-   console.log(changeOrigin([0,0]));
+   
 }; 
 
 function drawCircles() {
-   var svg = d3.select("#circleholder");
-   var moving = svg.append("g")
-      .classed("movingCircles", true); 
+   // square coords
+   var squareCoords = [
+      [origin[0] + circleRadius, origin[1] + circleRadius],
+      [origin[0] - circleRadius, origin[1] + circleRadius],
+      [origin[0] - circleRadius, origin[1] - circleRadius],
+      [origin[0] + circleRadius, origin[1] - circleRadius]
+   ]; 
+   var offset = circleRadius * 3 ** (1 / 2); 
+   var hexCoords = [
+      origin, 
+      [origin[0] + 2 * circleRadius, origin[1]], 
+      [origin[0] + circleRadius, origin[1] + offset], 
+      [origin[0] - circleRadius, origin[1] + offset],
+      [origin[0] - 2 * circleRadius, origin[1]],
+      [origin[0] - circleRadius, origin[1] - offset],
+      [origin[0] + circleRadius, origin[1] - offset],      
+   ]; 
+      
+   drawCirclesHelper(squareCoords, "squareGroup"); 
+   drawCirclesHelper(hexCoords, "hexagonGroup"); 
+}
+
+function drawCirclesHelper(coords, classname) {
+   var svg = d3.select("#" + classname);
+   var toMove = svg.append("g")
+      .classed(classname + "ToMove", true); 
    
-   // var origin = [75, 75];  
-   var coords = [[50,50],[100,50],[50,100],[100,100]];
-  
    coords.forEach(function(item, index) {    
       svg.append("circle")
          .attr("cx", item[0])
          .attr("cy", item[1])
-         .attr("r", 25)
+         .attr("r", circleRadius)
          .attr("fill", "none")
          .attr("stroke", "black")
-         .attr("stroke-width", 1.5); 
+         .attr("stroke-width", (1 / 25) * circleRadius); 
          
-      moving.append("circle")
+      toMove.append("circle")
          .attr("cx", item[0])
          .attr("cy", item[1])
-         .attr("r", 10)
-         .attr("fill", getRandColor([14,16]))
+         .attr("r", (2 / 5) * circleRadius)
+         .attr("fill", "blue")
          .classed("movable", true); 
    }); 
+}
 
+function rotateGroup(operations, classname) {
+   var type = operations[0]; 
+   var angle = parseInt(operations.replace(/\D/g,''));
+   
+   console.log(type);
+   console.log(angle); 
+   
+   var transString = ''; 
+   if (type == "r") {
+         // In three frames since otherwise the larger rotations transition in the opposite direction
+         var transString1 = "rotate(" + (angle / 3) + ")"; 
+         var transString2 = "rotate(" + (2 * angle / 3) + ")"; 
+         var transString3 = "rotate(" + (angle) + ")"; 
+         d3.select(classname).selectAll("circle")
+            // Reset, otherwise the transform will not be repeatable since the DOM state is same at start and end
+            .attr("transform", "rotate(0)")
+            .transition()
+               .duration(250)
+               .ease(d3.easeLinear)
+               .attr("transform-origin", "50 50")
+               .attr("transform", transString1)
+            .transition()
+               .duration(250)
+               .ease(d3.easeLinear)
+               .attr("transform-origin", "50 50")
+               .attr("transform", transString2)
+            .transition()
+               .duration(250)
+               .ease(d3.easeLinear)
+               .attr("transform-origin", "50 50")
+               .attr("transform", transString3)
+          
+   } else {
+      
+   }
+   
+   // fancy color change not working
+   // .each(function() {
+                  // d3.select(this)
+                     // .attr("fill", getRandColor([15,16]))
+               // })
+     // .transition()
+               // .duration(1000)
+            // .each(function() {
+                  // d3.select(this)
+                     // .attr("fill", "blue")
+               // })
+               
 }
 
 function reflect(angle) {
@@ -83,40 +161,6 @@ function reflect(angle) {
       // .attr("transform", "rotate(90, 75, 75)");
 }
 
-function changeOrigin(coords, areWindow = true) {
-   // areWindow == True means translate viewport coordinates to the origin of the system of objects.  If false, translate back to viewport coordinates. 
-   if (areWindow) {
-      coords[0] = coords[0] - origin[0]
-      coords[1] = origin[1] - coords[1];
-   } else {
-      coords[0] = (origin[0] + coords[0]); 
-      coords[1] = origin[1] - coords[1]; 
-   }
-   return coords; 
-}
-
-function rotateCoords(coords, angle) {
-   rotMat = [[Math.cos(angle), -1 * Math.sin(angle)], [Math.sin(angle), Math.cos(angle)]]; 
-   outx = rotMat[0][0] * coords[0] + rotMat[0][1] * coords[1]; 
-   outy = rotMat[1][0] * coords[0] + rotMat[1][1] * coords[1]; 
-
-   return [outx, outy]; 
-}
-
-function transformCoords(coords, angle, type, isX = true) {
-   coords = changeOrigin(coords, origin); 
-   
-   if (type == 'rotate') {
-      coords = rotateCoords(coords, angle); 
-   }
-   
-   coords = changeOrigin(coords, false); 
-   if (isX) {
-      return coords[0]; 
-   } else {
-      return coords[1]; 
-   }
-}
 
 function getRandColor(alpha=[0,16], red=[0,16], green=[0,16], blue=[0,16]) {
       var hexChars = "0123456789abcdef";

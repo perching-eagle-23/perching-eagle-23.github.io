36fc8cbde119ced582a8e5961973a88e9a6803b6
perching-eagle-23 <whom42@gmail.com>
2021-04-04 01:26:53 -0700

    static pages
    Saved an html version of each php page

--- templates/head.php
+++ templates/head.php
@@ -1,13 +1,18 @@
+<?php 
+   //echo realpath(''); 
+   $root = "http://localhost/learn/room202/persistence";
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
    <title><?= $pagetitle ?></title>
-   <link href="http://localhost/learn/persistence/math/main/styles/all.css" rel="stylesheet" type="text/css" />
+   <link href="<?= $root . '/styles/all.css' ?>" rel="stylesheet" type="text/css" />
    <!-- Load d3.js.  Even if there are no shiny d3 gizmos in a page, other scripts use d3 for concise element selection. -->
    <script src='https://d3js.org/d3.v6.js'></script>
    <!-- Navbar functionality -->
-   <script defer src="http://localhost/learn/persistence/math/main/styles/nav.js" type="text/javascript"></script>
+   <script defer src="<?= $root . '/styles/nav.js' ?>" type="text/javascript"></script>
    <?php
       // Include page specific scripts. 
       if (isset($pagescripts)) {
@@ -25,12 +30,10 @@
       </h1>
    </div>
    
-   <?php echo realpath(''); ?>
-   
    <nav id="mySidenav" class="sidenav">
       <a href="javascript:void(0)" class="closebtn">&times;</a>
-      <a href=".">Home</a>
-      <a href="./assignments">Assignments</a>
-      <a href="./topics">Topics</a>
-      <a href="./resources">Resources</a>
+      <a href="<?= $root ?>">Home</a>
+      <a href="<?= $root . '/assignments' ?>">Assignments</a>
+      <a href="<?= $root . '/topics' ?>">Topics</a>
+      <a href="<?= $root . '/resources' ?>">Resources</a>
    </nav>

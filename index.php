7e02fbccd2979b387b2f40b19528cdb1ce11678e
perching-eagle-23 <whom42@gmail.com>
2021-04-04 04:53:01 -0700

    trivial

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
    <?php 
       $pagetitle = 'Room 202 Math'; 
       $pagescripts = "<!-- The collapsible tree functionality -->
-         <script defer src='templates/collapsible.js' type='text/javascript'></script>";
+         <script defer src='" . $root . "/templates/collapsible.js' type='text/javascript'></script>";
       include("templates/head.php"); 
    ?>
    
@@ -9,7 +9,7 @@
       <div class="subwrapper">
          <h1>Curriculum</h2>
          <div>
-            <p>The overall learning objectives.  Click on a node to expand/collapse its component topics.  If on a small screen or not working, find the state's static version here: <a href="https://www.sde.idaho.gov/academic/shared/math/ICS-Mathematics.pdf" target="_blank">Idaho State Curriculum</a></p>
+            <p>The overall learning objectives.  Click on a node to expand/collapse its component topics.  If on a small screen or not working, find the state's static version <a href="https://www.sde.idaho.gov/academic/shared/math/ICS-Mathematics.pdf" target="_blank">here</a>.</p>
          </div>
       </div>
    </div>

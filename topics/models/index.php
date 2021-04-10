550b18b78b59919c1b883ece9e660cccd97a0977
room202math <whom42@gmail.com>
2021-04-10 10:14:50 -0700

    uniform dist demo
    rough bar chart graphic working to show dice roll frequencies.

--- topics/models/index.php
+++ topics/models/index.php
@@ -1,10 +1,11 @@
  <?php 
       $root = "https://room202math.github.io/";
+      $dev_root = "http://localhost/learn/room202/persistence/";
       $pagetitle = 'Topics / Symmetry'; 
       $pagescripts = "<!-- MathJax --> 
          <script async src='https://polyfill.io/v3/polyfill.min.js?features=es6'></script>
          <script async id='MathJax-script' src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>
-         <script defer src='" . $root . "topics/models/models.js' type='text/javascript'></script>"; 
+         <script defer src='" . $dev_root . "topics/models/models.js' type='text/javascript'></script>"; 
       include("../../templates/head.php"); 
    ?>
    
@@ -12,6 +13,8 @@
       <h1>Mathematical Modelling</h1>
       <p>The Greek philosopher Zeno posed the following argument in the 5th century BC.  An arrow shot at a target must travel half the distance, then half the remaining distance, then half of the distance remaining after that, and so on.  Since (according to this argument) there is always another half distance to go, how can the arrow ever reach the target?  
       </p>
+      
+      <div class="center_quote">\( \begin{eqnarray*} \sum_{i=1}^\infty \frac{1}{2^i} = \frac{1}{2} + \frac{1}{4} + \frac{1}{8} + ... = ? \end{eqnarray*} \)</div>
       
       <div class="inline_figure">
          <img src="../../figures/images/jan_baptist.jpg" alt="Jan Baptist Van Helmont waiting for scorpions to materialize" width="300px">
@@ -21,12 +24,34 @@
       It was once believed that small organisms formed spontaneously from nonliving stuff, ie fleas from dust.  This was called "spontaneous generation".  Jan Baptist van Helmont (1500's) helpfully determined that basil left between bricks in the sun would turn into scorpions. 
       </p>
       
-      <p>These are examples of models; in particular, bad ones.</p>
+      <p>These are examples of models, in particular bad ones.</p>
       
       <p class="definition">Mathematical model \( := \) A toy system which is posited to describe the behavior of some other system or phenomenon.</p>
       
-      <p>I choose "toys" not to belittle the importance of models but because they are ... things ... with behavior and state, little worlds of their own: toys, systems, mechanisms.  They do not necessarily have much to do with reality besides being concieved within it.  For example, the Boston Dynamics robots could be instructed to move in four spatial dimensions and this would be a perfectly valid conceptual model, but not a good model for our world, nor a good business decision.  The rules of chess and the names of pieces are a model; the fact that someone could pick up a physical piece and eat it does not obey this model.</p>
-     
+      <p>I choose "toys" not to belittle the importance of models but because they are ... things ... with behavior and state, little worlds of their own: toys, systems, mechanisms.  They do not necessarily have much to do with reality besides being concieved within it.  The rules of chess and the names of pieces are a model of chess play; the fact that someone could pick up a physical piece and eat it does not obey this model.</p>
+      
+      <p>In order to test how well a model describes something, we must make observations and/or measurements of that thing.  If the observations closely match what the model predicts, we say the model is a good model and give it a name, much like we would with a dog.</p>
+      
+      <p>Let's start quantifying the efficacy of models in describing phenomena with the following case study.</p>
+      
+      <h2 class="subsection" id="sub1">Section 1: Is this die a fair die?</h2>
+      <div id="sub1content">
+         <p>In the gizmo below, the blue bars tally the number of rolls of each value, while the green bars are fixed at equal numbers of rolls.  Roll a few times, and ask: is this a fair die?  Then roll a thousand, then a million times, and ask again.  (Obviously the "die" here is a piece of code, which makes the question more interesting.  The experiment tells us something useful - did I write a good die function?)</p>
+      
+         <div style="border:2px solid black;">
+            <svg id="die"></svg>
+         </div>
+         <button id="roll">Roll 1 time</button><button>Roll 1000 times</button><button>Roll 1000000 times</button>
+         <button id="showsteps">show steps</button><button id="clear">clear</button>
+         
+         <p>After a million rolls, you were probably convinced the die was fair.  But hopefully your takeaway after a few rolls was "I'm not sure, there is not enough data."  This is the crux of our study of statistics; the merit of our assessment of a model depends on how much quality data we collect.  This is why an anecdote is absolutely no grounds to, say, decide something about human nature.  One needs many, many data points to infer something about a large population.</p>
+         
+         <p>We'd like to measure how confident we are that the six sides of the die are equally likely - a model of the behavior of the die - which leads to the requirement of studying a little probability before statistics.  To measure our confidence in the hypothesis requires us to ask:</p>
+         
+         <div class="center_quote">
+            <p>Given that the hypothesis is true, how likely is it that the given data would be observed?</p>
+         </div>
+      </div>     
    </div>
 </body>
 </html>
--- topics/models/models.js
+++ topics/models/models.js
@@ -1,136 +1,90 @@
-var origin = [50,50];
-var circleRadius =  10; 
-
 window.onload = function(){
    // nav panel functions
    attachNavHandlers();
    
-   // the "Symmetry Transforms" graphic
-   drawCircles();
-   d3.select(".button_label8").selectAll("button").on("click", function() {
-      var operations = this.innerText; 
-      rotateGroup(this.innerText, ".squareGroupToMove");
+   let die = new Die();
+   drawGraphic();
+   d3.selectAll("button").on("click", function() {
+      // isolate number part from the pressed button's text, pass to the roll function
+      die.roll(parseInt(this.innerText.replace(/\D/g,'')));
+      updateGraphic(die.tally);
    }); 
-   
-   d3.select(".button_label12").selectAll("button").on("click", function() {
-      var operations = this.innerText; 
-      rotateGroup(this.innerText, ".hexagonGroupToMove");
+   d3.select("#showsteps").on("click", function() {
+      for (let i = 0; i < 10; i++) {
+         die.roll(); 
+         console.log(die.tally);
+         updateGraphic(die.tally); 
+      }
    }); 
-}; 
-
-function drawCircles() {
-   // square coords
-   var squareCoords = [
-      [origin[0] + circleRadius, origin[1] + circleRadius],
-      [origin[0] - circleRadius, origin[1] + circleRadius],
-      [origin[0] - circleRadius, origin[1] - circleRadius],
-      [origin[0] + circleRadius, origin[1] - circleRadius]
-   ]; 
-   var offset = circleRadius * 3 ** (1 / 2); 
-   var hexCoords = [
-      origin, 
-      [origin[0] + 2 * circleRadius, origin[1]], 
-      [origin[0] + circleRadius, origin[1] + offset], 
-      [origin[0] - circleRadius, origin[1] + offset],
-      [origin[0] - 2 * circleRadius, origin[1]],
-      [origin[0] - circleRadius, origin[1] - offset],
-      [origin[0] + circleRadius, origin[1] - offset],      
-   ]; 
-      
-   drawCirclesHelper(squareCoords, "squareGroup"); 
-   drawCirclesHelper(hexCoords, "hexagonGroup"); 
-}
-
-function drawCirclesHelper(coords, classname) {
-   var svg = d3.select("#" + classname);
-   var toMove = svg.append("g")
-      .classed(classname + "ToMove", true); 
-   
-   coords.forEach(function(item, index) {    
-      svg.append("circle")
-         .attr("cx", item[0])
-         .attr("cy", item[1])
-         .attr("r", circleRadius)
-         .attr("fill", "none")
-         .attr("stroke", "black")
-         .attr("stroke-width", (1 / 25) * circleRadius); 
-         
-      toMove.append("circle")
-         .attr("cx", item[0])
-         .attr("cy", item[1])
-         .attr("r", (2 / 5) * circleRadius)
-         .attr("fill", "blue")
-         .classed("movable", true); 
+   d3.select("#clear").on("click", function() {
+      updateGraphic('what', true); 
    }); 
 }
 
-function rotateGroup(operations, classname) {
-   var type = operations[0]; 
-   var angle = parseInt(operations.replace(/\D/g,''));
+function Die() {
+   this.tally = [0,0,0,0,0,0];
    
-   var t = d3.transition()
-      .duration(400)
-      .ease(d3.easeLinear); 
-   
-   var t_del = d3.transition()
-      .delay(1000)
-      .duration(300)
-      .ease(d3.easeLinear); 
-    
-   if (type == "r") {
-         // In three frames since otherwise the larger rotations transition in the opposite direction
-         var transString1 = "rotate(" + (angle / 3) + ")"; 
-         var transString2 = "rotate(" + (2 * angle / 3) + ")"; 
-         var transString3 = "rotate(" + (angle) + ")"; 
-
-         d3.select(classname).selectAll("circle")
-            // Reset, otherwise the transform will not be repeatable since the DOM state is same at start and end
-            .attr("transform", "rotate(0)")
-            .each(function( ){
-                  d3.select(this).transition(t).attr("fill", getRandColor([14,16]));
-            })
-            .transition(t_del)
-               .attr("transform-origin", "50 50")
-               .attr("transform", transString1)
-            .transition(t)
-               .attr("transform-origin", "50 50")
-               .attr("transform", transString2)
-            .transition(t)
-               .attr("transform-origin", "50 50")
-               .attr("transform", transString3)
-            .transition()
-               .delay(400)
-               .duration(300)
-               .attr("fill", "blue");
-          
-   } else {
-      d3.select(classname).selectAll("circle")
-         // Reset
-         .attr("transform", "scale(1)")
-         .each(function( ){
-            d3.select(this).transition(t).attr("fill", getRandColor([14,16]));
-         })
-         .transition(t_del)
-            .attr("transform-origin", "50 50")
-            .attr("transform", "rotate(" + angle + ")scale(-1,1)")
-         .transition()
-               .delay(400)
-               .duration(300)
-               .attr("fill", "blue");
-   }
-}
-
-function getRandColor(alpha=[0,16], red=[0,16], green=[0,16], blue=[0,16]) {
-   var hexChars = "0123456789abcdef";
-   var color = "#";
-   var agg = [red, green, blue, alpha]; 
-   // The parameters have syntax [minimum, range].  This allows for more green, less transparent, etc. color selection.
-   // Passing ([10,6],[10,4],[0,3],[4,5]), for example, gives Valentine's Day colors.
-   for (var i = 0; i < 4; i++) {
-      for (var j = 0; j < 2; j++) {
-         // Append the hex character at index miminum + x, where x is an integer less than range. 
-         color += hexChars.charAt(Math.floor(Math.random() * agg[i][1]) + agg[i][0]);
+   this.roll = function(count = 1) {
+      for (let i = 0; i < count; i++) {
+         let outcome = Math.floor(Math.random() * 6); 
+         this.tally[outcome] += 1;
       }
    }
-   return color; 
+}
+
+function drawGraphic() {
+   var width = "600"; 
+   var height = "500"; 
+   
+   var counts = [1,1,1,1,1,1]; 
+   var heightScale = d3.scaleLinear()
+      .domain([0, 1])
+      .range([0, height - 20]); 
+   
+   var svg = d3.select("svg#die")
+      .attr("width", width + "px")
+      .attr("height", height + "px");
+   svg.append('g')
+      .attr("transform", "translate(100,10)")
+      .classed("uniform", true);
+   svg.append('g')
+      .attr("transform", "translate(100,10)")
+      .classed("actual", true);
+   
+   d3.select(".uniform").selectAll("rect")
+      .data(counts)
+      .join("rect")
+         .attr("width", (width - 100) / 6)
+         .attr("height", function(d) {return heightScale(d);})
+         .attr("y", function(d) {return height - heightScale(d);})
+         .attr("x", function(d, i) {return i * ((width - 80) / 6) ;})
+         .attr("fill", "#33dd44aa");
+}
+
+function updateGraphic(tally, clear=false) {
+   var width = "600"; 
+   var height = "500"; 
+   var counts;
+   var heightScale; 
+   if (clear) {
+      counts = [0,0,0,0,0,0]; 
+      heightScale = d => 10;
+      console.log("clear");
+   } else {
+      counts = tally;
+      heightScale = d3.scaleLinear()
+         .domain([0, d3.max(counts)])
+         .range([0, height - 20]); 
+   }
+   
+   d3.select(".actual").selectAll("rect")
+      .data(counts)
+      .join("rect")
+         .transition()
+         .duration(400)
+         .attr("width", (width - 100) / 6)
+         .attr("height", function(d) {return heightScale(d);})
+         .attr("y", function(d) {return height - heightScale(d);})
+         .attr("x", function(d, i) {return i * ((width - 80) / 6) ;})
+         .attr("fill", "#8844dd99");
 }
